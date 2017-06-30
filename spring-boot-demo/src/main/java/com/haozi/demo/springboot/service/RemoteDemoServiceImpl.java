a7f0e7144f1cc0f8ab203a402930c4ffc190d64c
haoziapple <haozixiaowang@163.com>
2017-06-30 16:36:43 +0800

    使用@Service("/remoteService")的方式暴露服务会导致事务失效 建议使用HttpInvokerServiceExporter方式暴露服务

--- spring-boot-demo/src/main/java/com/haozi/demo/springboot/service/RemoteDemoServiceImpl.java
+++ spring-boot-demo/src/main/java/com/haozi/demo/springboot/service/RemoteDemoServiceImpl.java
@@ -6,6 +6,7 @@ import com.haozi.demo.springboot.bean.po.order.OrderBean;
 import com.haozi.demo.springboot.service.intf.RemoteDemoService;
 
 @Service
+//@Service("/remoteService")
 public class RemoteDemoServiceImpl implements RemoteDemoService
 {
 
