7cd826fac6a73381add179e28f7f0613e82fe4bb
haoziapple <haozixiaowang@163.com>
2017-07-24 19:01:52 +0800

    update spring demo:try redis publish & sub

--- spring-boot-demo/src/main/java/com/haozi/demo/springboot/controller/RedisController.java
+++ spring-boot-demo/src/main/java/com/haozi/demo/springboot/controller/RedisController.java
@@ -5,9 +5,16 @@ import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,9 +32,38 @@ import com.haozi.demo.springboot.bean.BookBean;
 
 @RestController
 @RequestMapping("/redis")
+@Configuration
 public class RedisController
 {
 	private final static Logger logger = LoggerFactory.getLogger(RedisController.class);
+
+	@Bean
+	public ChannelTopic topic()
+	{
+		return new ChannelTopic("pubsub:queue");
+	}
+
+	@Bean
+	public RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory, ChannelTopic topic)
+	{
+		final RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.addMessageListener(new MessageListener()
+		{
+
+			@Override
+			public void onMessage(Message message, byte[] pattern)
+			{
+				// TODO Auto-generated method stub
+				System.out.println("Message received: " + message.toString());
+			}
+		}, topic);
+
+		return container;
+	}
+
+	@Autowired
+	ChannelTopic topic;
 
 	@Autowired
 	StringRedisTemplate stringRedisTemplate;
@@ -82,6 +118,17 @@ public class RedisController
 	{
 		logger.debug("redis setJsonBean 操作:" + bookBean);
 		valOps.set(bookBean.getName(), bookBean);
+		return "success";
+	}
+
+	@RequestMapping(value = "/publish", method = RequestMethod.GET)
+	public String publish()
+	{
+		BookBean bean = new BookBean();
+		bean.setName("name");
+		bean.setAuthor("author");
+		bean.setPrice("price");
+		stringRedisTemplate.convertAndSend(topic.getTopic(), bean.toString());
 		return "success";
 	}
 }
