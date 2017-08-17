fc2c55229ffb1fdcaf2df2ea2a9a180f7b970147
haoziapple <haozixiaowang@163.com>
2017-08-17 11:37:20 +0800

    update

--- starter-parent/starter-service/src/main/java/com/fzrj/starter/service/common/aop/AdviceLog.java
+++ starter-parent/starter-service/src/main/java/com/fzrj/starter/service/common/aop/AdviceLog.java
@@ -8,7 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.Signature;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
