2ad3fa0d0c8adba0a79d640b73f19c58a55a035e
haoziapple <haozixiaowang@163.com>
2017-11-21 14:21:02 +0800

    调整定时任务的配置与测试方法

--- starter-parent/starter-service/src/main/java/com/fzrj/starter/service/controller/JobController.java
+++ starter-parent/starter-service/src/main/java/com/fzrj/starter/service/controller/JobController.java
@@ -1,7 +1,10 @@
 package com.fzrj.starter.service.controller;
 
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
+import com.aquatic.schedule.service.job.PostJob;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,20 +29,34 @@ import io.swagger.annotations.ApiOperation;
 @Api("定时任务api")
 @RestController
 @RequestMapping("/job")
-public class JobController
-{
-	private static final Logger logger = LoggerFactory.getLogger(JobController.class);
+public class JobController {
+    private static final Logger logger = LoggerFactory.getLogger(JobController.class);
 
-	@Autowired
-	private JobService jobService;
+    @Autowired
+    private JobService jobService;
 
-	@AddLog
-	@ApiOperation(value = "测试定时任务", notes = "测试新建一个固定的cron任务")
-	@RequestMapping(value = "/test", method = RequestMethod.POST)
-	public String test(@RequestBody String id)
-	{
-		jobService.addCronJob("jobName", "groupName", "triggerName", new HashMap<String, String>(),
-				"0 16/16 * * * ? *");
-		return "success";
-	}
+    @AddLog
+    @ApiOperation(value = "测试定时任务", notes = "测试新建一个固定的cron任务")
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    public String test(@RequestBody String id) {
+        Map<String,String> requestMap = new HashMap<>();
+        requestMap.put(PostJob.BODYKEY,"test request body");
+        requestMap.put(PostJob.URLKEY, "http://10.108.26.61:8081/starter_service/job/dest");
+        jobService.addCronJob("jobName", "groupName", "triggerName", requestMap,
+                "0/16 * * * * ? *");
+        return "success";
+    }
+
+    @RequestMapping(value = "/remove", method = RequestMethod.GET)
+    public String remove() {
+        jobService.removeJob("jobName", "triggerName");
+        return "success";
+    }
+
+    @RequestMapping(value = "/dest", method = RequestMethod.POST)
+    public String dest(@RequestBody String body) {
+        return new Date().toString() + body;
+    }
+
+
 }
